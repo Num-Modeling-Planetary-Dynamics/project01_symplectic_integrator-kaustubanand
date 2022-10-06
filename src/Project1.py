4b0e307d645417603e4ef660d687035093ab63eb
Kaustub Anand <kaustubanand@pal-10-184-107-166.itap.purdue.edu>
2022-10-06 12:53:08 -0400

    Test to add code from personal computer

---
+++ src/Project1.py
@@ -0,0 +1,22 @@
+# EAPS 591 - Project 1
+# Symplectic integrator to model the orbits of neptune and pluto
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# define a function for keplerian x_dot and y_dot
+def f():
+    return 10
+
+# define a function for keplerian v_x_dot and v_y_dot
+def g():
+    return 5
+
+# Main
+
+print("Hello")
+
+
+
+
